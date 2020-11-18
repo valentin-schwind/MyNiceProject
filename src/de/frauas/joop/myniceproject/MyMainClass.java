b8b6809dbcffbd080bebca221e505ac0484b7e79
Vali <Vali@KombiCompy.Kombinet>
2020-11-18 14:24:41 +0100

    My great for loop

--- src/de/frauas/joop/myniceproject/MyMainClass.java
+++ src/de/frauas/joop/myniceproject/MyMainClass.java
@@ -14,7 +14,9 @@ public class MyMainClass {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		for(int i = 0; i < 5; i++) {
+			System.out.println("i is " + i);
+		}
 	}
 
 }
