b8df7e4460126fa7ec06b193511c7d1137ec2743
Vali <Vali@KombiCompy.Kombinet>
2020-11-18 15:36:10 +0100

    Revert "My great for loop"
    This reverts commit b8b6809dbcffbd080bebca221e505ac0484b7e79.

--- src/de/frauas/joop/myniceproject/MyMainClass.java
+++ src/de/frauas/joop/myniceproject/MyMainClass.java
@@ -14,9 +14,7 @@ public class MyMainClass {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i = 0; i < 5; i++) {
-			System.out.println("i is " + i);
-		}
+		System.out.println("Hello World");
 	}
 
 }
